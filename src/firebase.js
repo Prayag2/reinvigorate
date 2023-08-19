cb540a9e52698607397146a5813822046e168648
Prayag Jain <prayagjain2@gmail.com>
2023-08-19 14:20:49 +0530

    build: changed the firebase project

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVdbB4I-Te5E7rAC5Wyxas35HHznr1U0A",
-  authDomain: "reinvigorate-foundation-ee274.firebaseapp.com",
-  projectId: "reinvigorate-foundation-ee274",
-  storageBucket: "reinvigorate-foundation-ee274.appspot.com",
-  messagingSenderId: "469651310618",
-  appId: "1:469651310618:web:4c76474a4d944643cde9a3"
+  apiKey: "AIzaSyBe9lnsisq6403eVoyfygnGu_fpWlpppiY",
+  authDomain: "ngo-reinvigorate-foundation.firebaseapp.com",
+  projectId: "ngo-reinvigorate-foundation",
+  storageBucket: "ngo-reinvigorate-foundation.appspot.com",
+  messagingSenderId: "549543359979",
+  appId: "1:549543359979:web:d7532d64fedd77b65b55ae",
+  measurementId: "G-79PYNDWCGF"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
